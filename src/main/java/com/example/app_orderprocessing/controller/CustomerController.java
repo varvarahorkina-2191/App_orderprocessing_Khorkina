c0e2c6a58d3a530efab6b5e99c4f336a84889615
Варвара <varvarahorkina@gmail.com>
2026-06-22 00:20:22 +0300

    Добавлено изменение заказчика

--- src/main/java/com/example/app_orderprocessing/controller/CustomerController.java
+++ src/main/java/com/example/app_orderprocessing/controller/CustomerController.java
@@ -17,6 +17,11 @@ public class CustomerController {
 
         customerView.getAddButton().setOnAction(event -> addCustomer());
         customerView.getDeleteButton().setOnAction(event -> deleteCustomer());
+        customerView.getEditButton().setOnAction(event -> updateCustomer());
+
+        customerView.getCustomerTable().setOnMouseClicked(
+                event -> fillFields()
+        );
     }
 
     public void loadCustomers() {
@@ -93,6 +98,84 @@ public class CustomerController {
         } else {
             customerView.getMessageLabel().setText(
                     "Не удалось удалить заказчика"
+            );
+        }
+    }
+
+    private void fillFields() {
+        Customer selectedCustomer =
+                customerView.getCustomerTable()
+                        .getSelectionModel()
+                        .getSelectedItem();
+
+        if (selectedCustomer != null) {
+            customerView.getNameField().setText(
+                    selectedCustomer.getCustomerName()
+            );
+
+            customerView.getAddressField().setText(
+                    selectedCustomer.getAddress()
+            );
+
+            customerView.getPhoneField().setText(
+                    selectedCustomer.getPhoneNumber()
+            );
+
+            customerView.getContactField().setText(
+                    selectedCustomer.getContactPerson()
+            );
+        }
+    }
+
+    private void updateCustomer() {
+        Customer selectedCustomer =
+                customerView.getCustomerTable()
+                        .getSelectionModel()
+                        .getSelectedItem();
+
+        if (selectedCustomer == null) {
+            customerView.getMessageLabel().setText(
+                    "Выберите заказчика"
+            );
+
+            return;
+        }
+
+        String name = customerView.getNameField().getText();
+        String address = customerView.getAddressField().getText();
+        String phone = customerView.getPhoneField().getText();
+        String contact = customerView.getContactField().getText();
+
+        if (name.isBlank()
+                || address.isBlank()
+                || phone.isBlank()
+                || contact.isBlank()) {
+
+            customerView.getMessageLabel().setText(
+                    "Заполните все поля"
+            );
+
+            return;
+        }
+
+        selectedCustomer.setCustomerName(name);
+        selectedCustomer.setAddress(address);
+        selectedCustomer.setPhoneNumber(phone);
+        selectedCustomer.setContactPerson(contact);
+
+        boolean updated =
+                customerDao.updateCustomer(selectedCustomer);
+
+        if (updated) {
+            customerView.getMessageLabel().setText(
+                    "Данные заказчика изменены"
+            );
+
+            clearFields();
+            loadCustomers();
+        } else {
+            customerView.getMessageLabel().setText(
+                    "Не удалось изменить заказчика"
             );
         }
     }
